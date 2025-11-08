ad6c01b53015cea33b4589b107ede59e1c302160
MiqueiasRBueno <160036158+MiqueiasRBueno@users.noreply.github.com>
2025-11-08 16:01:48 -0300

    exercíco número 2
    aula de logica de programação

---
+++ ex02.py
@@ -0,0 +1,7 @@
+# Somar 2 números
+
+print('\033[1;31mPrograma para somar 2 números!\033[m') #Título do programa
+num1 = float(input('Digite 1° número: ')) # Solicita ao usuário do 1° número para o usuário
+num2 = float(input('Digite 2° número: ')) # Solicita ao usuário do 2° número para o usuário
+area = float(num1 + num2) # Efetua a soma dos números inseridos
+print(f'A soma dos números digitados: {area}') # Mostra o resultado da soma
